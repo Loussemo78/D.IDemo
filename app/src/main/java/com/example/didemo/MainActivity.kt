73ef59001fb1d5fe52ddb6a491bef85652128fe2
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-04-19 20:20:33 +0200

    Initial commit

---
+++ app/src/main/java/com/example/didemo/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.didemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val smartPhone = SmartPhone(
+            Battery(),
+            SIMCard(ServiceProvider()),
+            MemoryCard()
+        )
+            .makeACallWithRecording()
+    }
+}
---
+++ app/src/main/java/com/example/didemo/SmartPhone.kt
@@ -0,0 +1,16 @@
+package com.example.didemo
+
+import android.util.Log
+
+class SmartPhone(val battery: Battery, val simCard: SIMCard, val memoryCard: MemoryCard) {
+    init {
+        battery.getPower()
+        simCard.getConnection()
+        memoryCard.getSpaceAvailablity()
+        Log.i("MYTAG", "SmartPhone Constructed")
+    }
+
+    fun makeACallWithRecording() {
+        Log.i("MYTAG", "Calling.....")
+    }
+}
