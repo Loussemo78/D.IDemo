c3551212ae1f087d4652e5f0ed09f51c29e48412
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-04-20 14:08:44 +0200

    state of a module example

--- app/src/main/java/com/example/didemo/MemoryCardModule.kt
+++ app/src/main/java/com/example/didemo/MemoryCardModule.kt
@@ -1,14 +1,16 @@
 package com.example.didemo
 
+import android.util.Log
 import dagger.Module
 import dagger.Provides
 
 
 @Module
-class MemoryCardModule {
+class MemoryCardModule(val memorySize:Int) {
 
     @Provides
     fun providesMemoryCard():MemoryCard{
+        Log.i("MYTAG","Size of the memory is $memorySize")
         return MemoryCard()
     }
 
